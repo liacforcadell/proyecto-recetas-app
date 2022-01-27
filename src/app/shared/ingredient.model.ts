ccfdc1c91e211c39751c92fa960ce2c190acb449
Lia <lia.cano@gmail.com>
2022-01-27 12:06:24 -0300

    Content for components added

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  /* IMPORTANTE -> shortcut to normal setup */
+  constructor(public name: string, public amount: number) {}
+
+  /*
+  public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+  */
+}
